d68f12946f914f2781ad3c7182be57dd777b44fc
hanhan1978 <ryo.tomidokoro@gmail.com>
2014-03-19 00:37:02 +0900

    remove hisotry function & free sql malloc

--- mssql.c
+++ mssql.c
@@ -7,44 +7,9 @@
 #include "mssql.h"
 
 #define HISTORY_FILE "~/.mssql_history" 
-char hisfilepath[256];
-
-void  sethisfilepath(){
-    strcpy(hisfilepath, getenv("HOME"));
-    char *filename = "/.mssql_history";
-    strcat(hisfilepath, filename);
-}
-
-void writehis(char * sql){
-    FILE *fp;
-    fp = fopen(hisfilepath, "a");
-    fputs(sql, fp);
-    fclose(fp);
-}
-
-void readhis(){
-    FILE *fp;
-    fp = fopen(hisfilepath, "r");
-    sql = (char *)malloc(2048);
-    if( fp == NULL ) {
-         printf( "file open error\n" );
-    }
-
-    fseek(fp, 5, SEEK_CUR);
-    int i;
-    while( fgets( sql , 1024 , fp ) != NULL ) {
-          break;
-//        printf( "row=%s" , sql );/* row */
-    }
-//    fgets(row, 1024, fp);
-    fclose(fp);
-}
 
 int main(void) {
-    sethisfilepath(); //set history filepath to global var hisfilepath
 
-//    readhis();
-//    printf("%s", sql);
 
     setlocale(LC_ALL,"");
     WINDOW * mainwin;
@@ -55,7 +20,7 @@ int main(void) {
 	    show_prompt();
         set_input();
         execute_query();
-        writehis(sql);
+        free(sql);
 	}
 
 	getch();
