4de8c754f2d2af485dadb7d17df9a773eea30b16
hanhan1978 <ryo.tomidokoro@gmail.com>
2015-01-14 00:33:57 +0900

    describe diarect now works

--- src/mssql.c
+++ src/mssql.c
@@ -96,6 +96,25 @@ char * trans_dialect(char * line){
         strcpy(sql,"SELECT name AS DBName FROM master.dbo.sysdatabases WHERE dbid > 4 ");
     }else if (slre_match("^show tables\\s*;\\s*$", line, strlen(line), caps, 4, SLRE_IGNORE_CASE) > 0) {
         strcpy(sql,"SELECT name AS Tables FROM sysobjects WHERE xtype = 'U'");
+    }else if (slre_match("^descr?i?b?e? ([^;\\s]+)\\s*;\\s*$", line, strlen(line), caps, 4, SLRE_IGNORE_CASE) > 0) {
+        sprintf(sql,"SELECT " 
+        "    c.name 'Column Name',"
+        "    t.Name 'Data type',"
+        "    c.max_length 'Max Length',"
+        "    c.precision ,"
+        "    c.scale ,"
+        "    c.is_nullable,"
+        "    ISNULL(i.is_primary_key, 0) 'Primary Key' "
+        "FROM    "
+        "    sys.columns c "
+        "INNER JOIN "
+        "    sys.types t ON c.user_type_id = t.user_type_id "
+        "LEFT OUTER JOIN "
+        "    sys.index_columns ic ON ic.object_id = c.object_id AND ic.column_id = c.column_id "
+        "LEFT OUTER JOIN "
+        "    sys.indexes i ON ic.object_id = i.object_id AND ic.index_id = i.index_id "
+        "WHERE"
+        "    c.object_id = OBJECT_ID('%.*s')", caps[0].len, caps[0].ptr);
     }else{
         strcpy(sql, line);
     }
