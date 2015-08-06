eda53f4139dbef290f32bec4330a22df5e83c415
hanhan1978 <ryo.tomidokoro@gmail.com>
2015-08-06 16:56:38 +0900

    query filter is complete

--- src/queryfilter.c
+++ src/queryfilter.c
@@ -8,15 +8,18 @@ char * trans_dialect(const char *); //public
 
 int regex_size();
 
-int main (){
-
-  char * output;
-  output = trans_dialect("show tables like 'fuga%'");
-  printf("sql -> %s\n", output);
-
-  output = trans_dialect("show tables");
-  printf("sql -> %s\n", output);
-}
+//int main (){
+//
+//  char * output;
+//  output = trans_dialect("show tables like 'fuga%'");
+//  printf("sql -> %s\n", output);
+//
+//  output = trans_dialect("show tables");
+//  printf("sql -> %s\n", output);
+//
+//  output = trans_dialect("desc hoge");
+//  printf("sql -> %s\n", output);
+//}
 
 typedef struct dialect{
     char *regex;
@@ -24,7 +27,7 @@ typedef struct dialect{
 } dialect;
 
 int regex_size(){
-  return 3;
+  return 6;
 }
 
 dialect * get_regex(){
@@ -33,19 +36,21 @@ dialect * get_regex(){
 
   int i = 0;
   for(i; i < reg_size; i++){
-    da[i].regex = (char *) malloc(1024);
-    da[i].sql = (char *) malloc(1024);
+    da[i].regex = (char *) malloc(512);
+    da[i].sql = (char *) malloc(2048);
   }
   strcpy(da[0].regex, "^show databases$");
   strcpy(da[0].sql, "SELECT name AS DBName FROM master.dbo.sysdatabases WHERE dbid > 4");
   strcpy(da[1].regex, "^show tables$");
   strcpy(da[1].sql, "SELECT name AS Tables FROM sysobjects WHERE xtype = 'U' ORDER BY name");
-  strcpy(da[2].regex, "^show tables like ([%'\"a-z]+)$");
+  strcpy(da[2].regex, "^show tables like ([%'\"a-z_-]+)$");
   strcpy(da[2].sql, "SELECT name AS Tables FROM sysobjects WHERE xtype = 'U' AND name LIKE %s ORDER BY name");
-  strcpy(da[3].regex, "");
-  strcpy(da[3].sql, "SELECT name AS Tables FROM sysobjects WHERE xtype = 'U' AND name LIKE %s ORDER BY name");
-  strcpy(da[4].regex, "^show tables like ([%'\"a-z]+)$");
-  strcpy(da[4].sql, "SELECT name AS Tables FROM sysobjects WHERE xtype = 'U' AND name LIKE %s ORDER BY name");
+  strcpy(da[3].regex, "^show processlist$");
+  strcpy(da[3].sql, "SELECT es.session_id AS sess_id, er.request_id AS req_id, er.command, es.status, DB_NAME(DB_ID()) AS database_name, er.wait_time, er.open_resultset_count AS result_set, es.total_elapsed_time AS elapsed_time, es.cpu_time, es.memory_usage, es.lock_timeout, es.host_name, es.program_name, es.login_name FROM sys.dm_exec_sessions es LEFT JOIN sys.dm_exec_requests er ON es.session_id = er.session_id LEFT JOIN (SELECT * FROM sys.dm_exec_connections WHERE most_recent_sql_handle <> 0x0) AS ec ON es.session_id = ec.session_id OUTER APPLY sys.dm_exec_sql_text(er.sql_handle) AS er_text OUTER APPLY sys.dm_exec_sql_text(ec.most_recent_sql_handle) AS ec_text WHERE es.session_id <> @@SPID ORDER BY sess_id ASC");
+  strcpy(da[4].regex, "^desc ([%'\"a-z_-]+)$");
+  strcpy(da[4].sql, "SELECT c.name 'Column Name', t.Name 'Data type', c.max_length 'Max Length', c.is_nullable 'NULL', c.collation_name 'COLLATE', ISNULL(i.is_primary_key, 0) 'Primary Key' FROM sys.columns c INNER JOIN sys.types t ON c.user_type_id = t.user_type_id LEFT OUTER JOIN sys.index_columns ic ON ic.object_id = c.object_id AND ic.column_id = c.column_id AND ic.index_id=1 LEFT OUTER JOIN sys.indexes i ON ic.object_id = i.object_id AND ic.index_id = i.index_id WHERE c.object_id = OBJECT_ID('%s')");
+  strcpy(da[5].regex, "^describe ([%'\"a-z_-]+)$");
+  strcpy(da[5].sql, "SELECT c.name 'Column Name', t.Name 'Data type', c.max_length 'Max Length', c.is_nullable 'NULL', c.collation_name 'COLLATE', ISNULL(i.is_primary_key, 0) 'Primary Key' FROM sys.columns c INNER JOIN sys.types t ON c.user_type_id = t.user_type_id LEFT OUTER JOIN sys.index_columns ic ON ic.object_id = c.object_id AND ic.column_id = c.column_id AND ic.index_id=1 LEFT OUTER JOIN sys.indexes i ON ic.object_id = i.object_id AND ic.index_id = i.index_id WHERE c.object_id = OBJECT_ID('%s')");
   return da;
 }
 
@@ -70,7 +75,6 @@ char * trans_dialect(const char * input){
             matched[k] = *(input + match[1].rm_so + k);
           }
           matched[k] = '\0';
-          //printf("k -> %d, matched -> %s \n", k, matched);
           output_length += match[1].rm_eo - match[1].rm_so;
           output =(char *)malloc(output_length * sizeof(char));
           sprintf(output, ds[i].sql, matched);
--- test/queryfilter.cpp
+++ test/queryfilter.cpp
@@ -11,20 +11,22 @@ using ::testing::StrEq;
 TEST(QueryTranslateTest, ShowTables)
 {
     const char query[] ="show tables";
-    EXPECT_THAT(trans_dialect(query), StartsWith("SELECT name AS Tables FROM sysobjects"));
+    EXPECT_THAT(trans_dialect(query), StrEq("SELECT name AS Tables FROM sysobjects WHERE xtype = 'U' ORDER BY name"));
 
     const char query2[] ="show databases";
-    EXPECT_THAT(trans_dialect(query2), StartsWith("SELECT name AS DBName FROM master.dbo.sysdatabases WHERE dbid > 4"));
+    EXPECT_THAT(trans_dialect(query2), StrEq("SELECT name AS DBName FROM master.dbo.sysdatabases WHERE dbid > 4"));
 
     const char query3[] ="show tables like 'hoge%'";
-    EXPECT_THAT(trans_dialect(query3), StartsWith("SELECT name AS Tables FROM sysobjects WHERE xtype = 'U' AND name LIKE %s ORDER BY name"));
+    EXPECT_THAT(trans_dialect(query3), StrEq("SELECT name AS Tables FROM sysobjects WHERE xtype = 'U' AND name LIKE 'hoge\%' ORDER BY name"));
 
     const char query4[] ="show processlist";
-    EXPECT_THAT(trans_dialect(query4), StartsWith("SELECT es.session_id AS sess_id, er.request_id AS req_id, er.command, es.status, DB_NAME(DB_ID()) AS database_name, er.wait_time, er.open_resultset_count AS result_set, es.total_elapsed_time AS elapsed_time, es.cpu_time, es.memory_usage, es.lock_timeout, es.host_name, es.program_name, es.login_name FROM sys.dm_exec_sessions es LEFT JOIN sys.dm_exec_requests er ON es.session_id = er.session_id LEFT JOIN (SELECT * FROM sys.dm_exec_connections WHERE most_recent_sql_handle <> 0x0) AS ec ON es.session_id = ec.session_id OUTER APPLY sys.dm_exec_sql_text(er.sql_handle) AS er_text OUTER APPLY sys.dm_exec_sql_text(ec.most_recent_sql_handle) AS ec_text WHERE es.session_id <> @@SPID ORDER BY sess_id ASC"));
+    EXPECT_THAT(trans_dialect(query4), StrEq("SELECT es.session_id AS sess_id, er.request_id AS req_id, er.command, es.status, DB_NAME(DB_ID()) AS database_name, er.wait_time, er.open_resultset_count AS result_set, es.total_elapsed_time AS elapsed_time, es.cpu_time, es.memory_usage, es.lock_timeout, es.host_name, es.program_name, es.login_name FROM sys.dm_exec_sessions es LEFT JOIN sys.dm_exec_requests er ON es.session_id = er.session_id LEFT JOIN (SELECT * FROM sys.dm_exec_connections WHERE most_recent_sql_handle <> 0x0) AS ec ON es.session_id = ec.session_id OUTER APPLY sys.dm_exec_sql_text(er.sql_handle) AS er_text OUTER APPLY sys.dm_exec_sql_text(ec.most_recent_sql_handle) AS ec_text WHERE es.session_id <> @@SPID ORDER BY sess_id ASC"));
     
     const char query5[] ="describe hoge";
-    EXPECT_THAT(trans_dialect(query5), StartsWith("SELECT c.name 'Column Name', t.Name 'Data type', c.max_length 'Max Length', c.is_nullable 'NULL', c.collation_name 'COLLATE', ISNULL(i.is_primary_key, 0) 'Primary Key' FROM sys.columns c INNER JOIN sys.types t ON c.user_type_id = t.user_type_id LEFT OUTER JOIN sys.index_columns ic ON ic.object_id = c.object_id AND ic.column_id = c.column_id AND ic.index_id=1 LEFT OUTER JOIN sys.indexes i ON ic.object_id = i.object_id AND ic.index_id = i.index_id WHERE c.object_id = OBJECT_ID('%s')"));
+    EXPECT_THAT(trans_dialect(query5), StrEq("SELECT c.name 'Column Name', t.Name 'Data type', c.max_length 'Max Length', c.is_nullable 'NULL', c.collation_name 'COLLATE', ISNULL(i.is_primary_key, 0) 'Primary Key' FROM sys.columns c INNER JOIN sys.types t ON c.user_type_id = t.user_type_id LEFT OUTER JOIN sys.index_columns ic ON ic.object_id = c.object_id AND ic.column_id = c.column_id AND ic.index_id=1 LEFT OUTER JOIN sys.indexes i ON ic.object_id = i.object_id AND ic.index_id = i.index_id WHERE c.object_id = OBJECT_ID('hoge')"));
 
+    const char query6[] ="desc hoge";
+    EXPECT_THAT(trans_dialect(query6), StrEq("SELECT c.name 'Column Name', t.Name 'Data type', c.max_length 'Max Length', c.is_nullable 'NULL', c.collation_name 'COLLATE', ISNULL(i.is_primary_key, 0) 'Primary Key' FROM sys.columns c INNER JOIN sys.types t ON c.user_type_id = t.user_type_id LEFT OUTER JOIN sys.index_columns ic ON ic.object_id = c.object_id AND ic.column_id = c.column_id AND ic.index_id=1 LEFT OUTER JOIN sys.indexes i ON ic.object_id = i.object_id AND ic.index_id = i.index_id WHERE c.object_id = OBJECT_ID('hoge')"));
 }
 
 
