8ae31ae5c7e44885cabd7521781161462f8d7544
hanhan1978 <ryo.tomidokoro@gmail.com>
2015-08-18 10:30:00 +0900

    add comment

--- src/output.c
+++ src/output.c
@@ -1,50 +1,36 @@
+/** @file
+ * output.c 
+ *
+ * define functions related to output query result
+ * */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "mssql.h"
 
 
-//private
+//private funcs
 void _print_pretty(struct result_set * res);
 void _print_normal(struct result_set * res);
 void _print_normal_result(struct result_node * node, int * col_length, int col_size);
 void _print_normal_boundary(int * col_length, int col_size);
 
-
-/*
-int main (){
-  int a[4] = {1,2,3,6};
-
-  struct result_node * head = NULL;
-  struct result_node * temp = NULL;
-  temp = add_node("h", temp, 1024);
-  head = temp;
-  temp = add_node("ho", temp, 1024);
-  temp = add_node("hog", temp, 1024);
-  temp = add_node("hoge", temp, 1024);
-
-  struct result_node * head2 = NULL;
-  struct result_node * temp2 = NULL;
-  temp2 = add_node("1", temp2, 1024);
-  head2 = temp2;
-  temp2 = add_node("wo", temp2, 1024);
-  temp2 = add_node("yos", temp2, 1024);
-  temp2 = add_node("hoge", temp2, 1024);
-  temp2 = add_node("2", temp2, 1024);
-  temp2 = add_node("ho", temp2, 1024);
-  temp2 = add_node("hog", temp2, 1024);
-  temp2 = add_node("あい", temp2, 1024);
-
-  struct result_set * res = (struct result_set *)malloc( sizeof(struct result_set) );
-  res->colnum = 4;
-  res->each_collen = a;
-  res->hnode = head;
-  res->node = head2;
-  print_normal(res);
-  print_pretty(res);
-  return 1;
+//public
+void print_result(struct result_set * res){
+  if(res->rows == 0 && res->hnode == NULL){
+    return;
+  }
+  if(pretty_print){
+      _print_pretty(res);
+  }else{
+      _print_normal(res);
+  }
+  if(res->rows > 0){
+      printf("\n%d %s in set\n\n", res->rows, res->rows <= 1 ? "row" : "rows");
+  }else{
+      printf("Empty set\n\n");
+  }
 }
-*/
 
 void _print_normal_boundary(int * col_length, int col_size){
     int col = 0;
@@ -78,25 +64,6 @@ void _print_normal_result(struct result_node * node, int * col_length, int col_s
     }
 }
 
-void print_result(struct result_set * res){
-
-  if(res->rows == 0 && res->hnode == NULL){
-    return;
-  }
-
-  if(pretty_print){
-      _print_pretty(res);
-  }else{
-      _print_normal(res);
-  }
-
-  if(res->rows > 0){
-      printf("\n%d %s in set\n\n", res->rows, res->rows <= 1 ? "row" : "rows");
-  }else{
-      printf("Empty set\n\n");
-  }
-  
-}
 
 void _print_normal(struct result_set * res){
       _print_normal_boundary(res->each_collen, res->colnum);
